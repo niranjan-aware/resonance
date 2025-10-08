271aa0c869329e3f227d1089025103defbab8f61
NiranjanVdojar <niranjan@vdojar.com>
2025-10-08 09:43:39 +0530

    render hosting1.14

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(), react()
-  ],
+  base: '/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
